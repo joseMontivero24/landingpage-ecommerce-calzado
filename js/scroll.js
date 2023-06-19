ec84ff3a42dc6e1da85f9f838622124414dd693f
jose montivero <chipimontivero2019@gmail.com>
2023-06-19 20:56:47 -0300

    metiendo mas cambios

---
+++ js/scroll.js
@@ -0,0 +1,5 @@
+sr.reveal('.cabecera-swiper, .about-swiper, .footer-swiper')
+// sr.reveal('.about-swiper')
+sr.reveal('.choose-swiper, .zapa-swiper',{interval: 100})
+sr.reveal('.left-swiper, .left-form-swiper, .left', {origin: 'left'})
+sr.reveal('.right-swiper, .right-form-swiper, .new-swiper', {origin: 'right'})
