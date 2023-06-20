8e14a44b98f7303e46364f9c7663c214a57f3216
jose montivero <chipimontivero2019@gmail.com>
2023-06-19 21:20:23 -0300

    agregando funcionalidad en js, click-cierre

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const menuList = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 
-
 menuBtn.onclick = () => {
     menuList.classList.add("active");
     menuBtn.classList.add("hide");
@@ -15,11 +14,19 @@ cancelBtn.onclick = () => {
     menuBtn.classList.remove("hide");
 }
 
+// Agrega el siguiente código para cerrar el menú al hacer clic en una opción
+const menuOptions = document.querySelectorAll(".menu-list-item");
+
+menuOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        menuList.classList.remove("active");
+        menuBtn.classList.remove("hide");
+    });
+});
+
 window.onscroll = () => {
     this.scrollY > 200 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
-
-/*================== Scroll Reveal Animation =================*/ 
 
 
 
